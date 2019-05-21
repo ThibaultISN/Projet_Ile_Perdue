817c94931abc3c5f430b90d2f385de5cd1352f72
remulef <remulef@pc-dg-033-11.iut2.upmf-grenoble.fr>
2019-05-21 18:14:44 +0200

    Merge origin/main into Fabien
    Conflicts:
    	src/projet_ile_perdue/Projet_Ile_Perdue.java

--- src/Tuile.java
+++ src/Tuile.java
@@ -3,49 +3,42 @@ import java.util.ArrayList;
 
 public class Tuile {
 
-	private String nom;
-	private Trésor trésor;
-        private Position position;
-        private EtatTuile etatTuile;
-        
+    private String nom;
+    private Trésor trésor;
+    private Position position;
+    private EtatTuile etatTuile;
 
-	/**
-	 * 
-	 * @param nom
-	 */
-	public Tuile(String nom, Trésor trésor , Position position) { //Pour les tuiles disposant d'un trésor
-		setNom(nom);
-                setTrésor(trésor);
-                setPosition(position);
-                etatTuile=EtatTuile.seche;
-                setEtatTuile(etatTuile);
-		
-	}
-        
-        public Tuile(String nom,Position position) { //Pour les tuiles non nul
-		setNom(nom);
-                
-                setPosition(position);
-                
-                etatTuile=EtatTuile.seche;
-                setEtatTuile(etatTuile);
-		
-	}
-        
-        public Tuile(Position position) { //Pour les tuiles vide
-		
-                
-                setPosition(position);
-                
-                etatTuile=EtatTuile.vide;
-                setEtatTuile(etatTuile);
-		
-	}
-        
-        
-        
-        
-         
+    /**
+     *
+     * @param nom
+     */
+    public Tuile(String nom, Trésor trésor, Position position) { //Pour les tuiles disposant d'un trésor
+        setNom(nom);
+        setTrésor(trésor);
+        setPosition(position);
+        etatTuile = EtatTuile.seche;
+        setEtatTuile(etatTuile);
+
+    }
+
+    public Tuile(String nom, Position position) { //Pour les tuiles non nul
+        setNom(nom);
+
+        setPosition(position);
+
+        etatTuile = EtatTuile.seche;
+        setEtatTuile(etatTuile);
+
+    }
+
+    public Tuile(Position position) { //Pour les tuiles vide
+
+        setPosition(position);
+
+        etatTuile = EtatTuile.vide;
+        setEtatTuile(etatTuile);
+
+    }
 
     public EtatTuile getEtatTuile() {
         return etatTuile;
@@ -54,8 +47,6 @@ public class Tuile {
     public void setEtatTuile(EtatTuile etatTuile) {
         this.etatTuile = etatTuile;
     }
-        
-        
 
     public String getNom() {
         return nom;
@@ -81,21 +72,38 @@ public class Tuile {
         this.position = position;
     }
 
+    public Position générateurpositionrandom() {
+        Position randompos = new Position();
+        int Min = 0;
+        int Max = 6;
+
+        randompos.setX(Min + (int) (Math.random() * ((Max - Min) + 1)));
+        randompos.setY(Min + (int) (Math.random() * ((Max - Min) + 1)));
+
+        return randompos;
+
+    }
+
+    public boolean estcontigue(Tuile tuile) {
+        Position pos = this.position;
+        Position param = tuile.position;
+
+        int paramX = tuile.position.getX();
+        int paramY = tuile.position.getY();
+
+        ArrayList<Position> listconti = new ArrayList<>(); // liste des position contigue de la tuile en parametre 
+
+        listconti.add(new Position(paramX + 1, paramY));
+        listconti.add(new Position(paramX, paramY + 1));
+        listconti.add(new Position(paramX - 1, paramY));
+        listconti.add(new Position(paramX, paramY - 1));
 
         
-        public Position générateurpositionrandom(){
-        Position randompos = new Position();
-        int Min = 0 ;
-        int Max = 6 ;
-        
-        randompos.setX( Min + (int)(Math.random() * ((Max - Min) + 1)) ); 
-        randompos.setY( Min + (int)(Math.random() * ((Max - Min) + 1)) );
-        
-        return randompos;
-             
-    }
-        
+        return (listconti.contains(this.position));
+
     
 
+    }
 }
+        
 
