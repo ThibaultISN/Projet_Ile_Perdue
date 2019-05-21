bbceec220b87b4f6c6194a5e5687ae3e0d05afb1
glandutt <glandutt@pc-dg-033-12.iut2.upmf-grenoble.fr>
2019-05-21 18:04:44 +0200

    test nouvelle branche

--- src/Aventurier.java
+++ src/Aventurier.java
@@ -3,7 +3,7 @@ import java.util.*;
 public abstract class Aventurier {
 
 	Collection<CarteTresor> carte;
-	Tuile tuiles;
+	private Tuile emplacement;
 	private String Nom;
 	private int nbAction;
 	private Boolean enVie;
@@ -45,5 +45,75 @@ public abstract class Aventurier {
 		// TODO - implement Aventurier.prendretrésor
 		throw new UnsupportedOperationException();
 	}
+
+    /**
+     * @return the emplacement
+     */
+    public Tuile getEmplacement() {
+        return emplacement;
+    }
+
+    /**
+     * @param emplacement the emplacement to set
+     */
+    public void setEmplacement(Tuile emplacement) {
+        this.emplacement = emplacement;
+    }
+
+    /**
+     * @return the Nom
+     */
+    public String getNom() {
+        return Nom;
+    }
+
+    /**
+     * @param Nom the Nom to set
+     */
+    public void setNom(String Nom) {
+        this.Nom = Nom;
+    }
+
+    /**
+     * @return the nbAction
+     */
+    public int getNbAction() {
+        return nbAction;
+    }
+
+    /**
+     * @param nbAction the nbAction to set
+     */
+    public void setNbAction(int nbAction) {
+        this.nbAction = nbAction;
+    }
+
+    /**
+     * @return the enVie
+     */
+    public Boolean getEnVie() {
+        return enVie;
+    }
+
+    /**
+     * @param enVie the enVie to set
+     */
+    public void setEnVie(Boolean enVie) {
+        this.enVie = enVie;
+    }
+
+    /**
+     * @return the thibault
+     */
+    public String getThibault() {
+        return thibault;
+    }
+
+    /**
+     * @param thibault the thibault to set
+     */
+    public void setThibault(String thibault) {
+        this.thibault = thibault;
+    }
 
 }
