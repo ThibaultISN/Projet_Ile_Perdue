725437cc16e06d4a7a113345ac223ab8ba6a4140
Remulef <fabienremule974@gmail.com>
2019-05-27 17:09:28 +0200

    Update testgénératiodestuiles.java

--- src/testgénératiodestuiles.java
+++ src/testgénératiodestuiles.java
@@ -1,5 +1,14 @@
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -25,6 +34,24 @@ public class testgénératiodestuiles {
         for(Tuile t : l ){
             t.affiche();
         }
+         while(true){
+		// test
+		try {
+                    AudioInputStream audioIn = AudioSystem.getAudioInputStream(new File(
+                            "damso-seultou-son-officiel.wav"));
+                    // Get a sound clip resource.
+                    Clip clip = AudioSystem.getClip();
+                    // Open audio clip and load samples from the audio input stream.
+                    clip.open(audioIn);
+                } catch (UnsupportedAudioFileException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (LineUnavailableException e) {
+                    e.printStackTrace();
+                }
+         }
+        
         
     }
     
