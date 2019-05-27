7ff02ea2c84e8cfe856b84eb9288b60aa0b1c8b0
clementlopez <clementlopez@172.20.10.4>
2019-05-27 11:46:25 +0200

    Modif Clément

--- src/Controleur.java
+++ src/Controleur.java
@@ -1,72 +1,82 @@
+
 import java.util.*;
 
 public class Controleur {
 
-	Grille grille;
-	Collection<Aventurier> joueurs;
-	Collection<CarteTresor> cartes;
-	Collection<CarteInondation> cartesinnond;
-	Echelle echelle;
+    Grille grille;
+    Collection<Aventurier> joueurs;
+    Collection<CarteTresor> cartes;
+    Collection<CarteInondation> cartesinnond;
+    Echelle echelle;
+    int a;
 
-	/**
-	 *
-	 * @param nomjoueur
-	 * @param cartetrésor
-	 */
-        
-        //il faut 2 joueurs
-	public boolean PossibleDon(Aventurier nomjoueur, CarteTresor cartetrésor) {
-		// TODO - implement Controleur.PossibleDon
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 *
-	 * @param tuile
-	 * @param role
-	 */
-	public boolean Possibleasseche(Tuile tuile, Aventurier role) {
-           //voir code de Clément
-		throw new UnsupportedOperationException();
-	}
-
-	
-
-	/**
-	 *
-	 * @param joueur
-	 * @param tresor
-	 */
-	public boolean Possibleprisetrésor(Aventurier joueur, Trésor tresor) {
-           //voir code de Clément
-            
-            for(CarteTresor c : joueur.cartes){
-                if(c.getT() == tresor)
-                
+    /**
+     *
+     * @param nomjoueur
+     * @param cartetrésor
+     */
+    public boolean PossibleDon(Aventurier joueurRec, Aventurier joueurEnv, CarteTresor cartetrésor) {
+        // TODO - implement Controleur.PossibleDon
+        if (joueurEnv.getRole() == "Messager") {
+            if (joueurEnv.carte.contains(cartetrésor)) {
+                return true;
+            } else {
+                return false;
             }
-            
-            
-		
-	}
+        } else {
+            if (joueurEnv.getTuile() == joueurRec.getTuile() && joueurEnv.carte.contains(cartetrésor)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 
+    /**
+     *
+     * @param tuile
+     * @param role
+     */
+    public boolean Possibleasseche(Tuile tuile, Aventurier x) {
+        // TODO - implement Controleur.Possibleasseche
+        if (x.getRole() == "Explorateur") {
+            if ((x.estContigue(tuile) || x.estDiagonale(tuile)) && tuile.getEtatTuile() == innonde) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (x.estContigue(tuile) && tuile.getEtatTuile() == innonde) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 
-        public void ChoixDifficultés(){
-                                Scanner sc = new Scanner(System.in);
+    /**
+     *
+     * @param joueur
+     * @param tresor
+     */
+    public boolean Possibleprisetrésor(Aventurier joueur, Trésor tresor) {
+        // TODO - implement Controleur.Possibleprisetrésor
 
-            System.out.println ("Quel difficulté voulez vous ? (Novice/Normal/Elite/Legendaire) \n");
-
-            String str = sc.nextLine();
-
-            while(              !"Novice".equals(str) ||!"Normal".equals(str) ||!"Elite".equals(str) ||!"Legendaire".equals(str)  ){
-            System.out.println ("Veuillez saisir une difficulté proposer \n");
-            System.out.println ("Quel difficulté voulez vous ? (Novice/Normal/Elite/Legendaire) \n");
-
-             str = sc.nextLine();
+        for (CarteTresor c : joueur.cartes) {
+            if (c.getT() == tresor) {
+                i = i + 1;
+            }
+        }
+        if (i >= 4) {
+            for (Tuile t : grille.TuileTresor(tresor)) {
+                if (joueur.getTuile() == t && t.getEtatTuile() != disparue) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        } else {
+            return false;
         }
 
-            Echelle ech = new Echelle(str);
-            System.out.println("La difficulté a été initialisé à " + str +"`\n"
-                    +" Niveau de l'eau :" + ech.getGraduation() + "\n");
-
-        }
-}
+    }
